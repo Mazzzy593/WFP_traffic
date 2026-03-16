c9eb2235eb6cd6ca94ecad450ea115f2db58b8bf
lunan0320@github.com <lunan0320@163.com>
2026-03-16 23:35:33 +0800

    TCPandIPfragment

--- win-shaper/driver/IP_callout.c
+++ win-shaper/driver/IP_callout.c
@@ -260,4 +260,175 @@ void updateTCPchecksum(PUCHAR pIPHeader, PUCHAR pTCPHeader, PUCHAR pPayload, UIN
 }
 
 
+VOID updateTCPSeqNumber(PUCHAR tcpPacket,UINT alreadyCopied)
+{
+	PUCHAR pSeqNumber = tcpPacket + 4;
+	UINT currentSeqNumber = (UINT)(pSeqNumber[0] << 24 | pSeqNumber[1] << 16 | pSeqNumber[2] << 8 | pSeqNumber[3]);
 
+	currentSeqNumber += alreadyCopied;
+	PUCHAR tmp = (PUCHAR)&currentSeqNumber;
+
+	pSeqNumber[0] = tmp[3];
+	pSeqNumber[1] = tmp[2];
+	pSeqNumber[2] = tmp[1];
+	pSeqNumber[3] = tmp[0];
+}
+
+BOOLEAN TCPandIPfragment(_In_ const FWPS_INCOMING_VALUES* inFixedValues,   // 经过包装的数据包的相关信息， 有layer ID, source and destination addresses
+	_In_ const FWPS_INCOMING_METADATA_VALUES* inMetaValues,   // 经过包装的数据信息，interface index, timestamps
+	_Inout_opt_ void* layerData,
+	BOOLEAN outbound, // 标志位
+	_In_ HANDLE injection_handle) 
+{
+#ifdef DEBUG
+	if (Debugflags)
+	{
+		DbgBreakPoint();
+		Debugflags = FALSE;
+	}
+#endif
+	NTSTATUS status;
+	
+	LARGE_INTEGER now = KeQueryPerformanceCounter(NULL);
+	ULONG random = RtlRandomEx(&now.LowPart) % 100;
+	if (random > SELF_TCPFragmentRate)
+		return FALSE;
+
+	ULONG randomMSS = SELF_MSSmin + RtlRandomEx(&now.LowPart) % ((SELF_MSSmax - SELF_MSSmin)>0? (SELF_MSSmax - SELF_MSSmin):1);
+
+	PNET_BUFFER_LIST CopyLayerdata = NULL;
+	TL_INSPECT_PENDED_PACKET* pendedPacket = NULL;
+	PIPV4_HEADER ipHeaderInfo = NULL;
+	pendedPacket = AllocateAndInitializePendedPacket(
+		inFixedValues,
+		inMetaValues,
+		layerData
+	);
+
+	// 解析IP和TCP包头
+	PNET_BUFFER nb = NET_BUFFER_LIST_FIRST_NB((PNET_BUFFER_LIST)layerData);
+	PNET_BUFFER currentNB = NULL;	// 这里的currentNB是指当前构建的新NET_BUFFER_LIST中最后一个NET_BUFFER
+
+	PVOID currentMdlStart = MmGetSystemAddressForMdlSafe(NET_BUFFER_CURRENT_MDL(nb), NormalPagePriority);
+	PUCHAR NetBufferData = (PUCHAR)currentMdlStart + NET_BUFFER_CURRENT_MDL_OFFSET(nb);
+	ipHeaderInfo = (PIPV4_HEADER)ExAllocatePoolWithTag(NonPagedPool, sizeof(IPV4_HEADER), TCPCALLOUT_POOL_TAG);
+	IPV4_HEADER_INIT(ipHeaderInfo, NetBufferData);		
+
+	// 1. 判断协议类型
+	if (ipHeaderInfo->Protocol != IPH_TCP) {
+		goto EXIT;
+	}
+	// 2. 判断数据包长度是否需要分片	
+	if (ipHeaderInfo->TotalLength < SELF_MSSmax) {
+		goto EXIT;
+	}
+	UINT16 IPHeaderLen = ipHeaderInfo->HeaderLength;
+
+	NdisAdvanceNetBufferDataStart(nb, IPHeaderLen, 0, 0);
+	PTCP_HEADER pTCPHeader = NdisGetDataBuffer(nb, sizeof(TCP_HEADER), NULL, 1, 0);
+	NdisRetreatNetBufferDataStart(nb, IPHeaderLen, 0, 0);
+	UINT TCPHeaderLen = pTCPHeader->dORNS.dataOffset * 4;
+
+	///////////////////////////////////////////////////////////////////////////////////////////
+	//  TCPfragement
+	///////////////////////////////////////////////////////////////////////////////////////////
+	// 
+	// 分片用到的参数：
+	
+	ULONG payloadLen = ipHeaderInfo->TotalLength - IPHeaderLen - TCPHeaderLen;
+	ULONG remainDataLen = payloadLen;	// 剩余字节长度
+	UINT32 currentSeqNum = pTCPHeader->sequenceNumber;
+	
+	CopyLayerdata = (PNET_BUFFER_LIST)ExAllocatePoolWithTag(NonPagedPool, sizeof(NET_BUFFER_LIST), TCPCALLOUT_POOL_TAG);
+
+	//分配一个新NBL，带有NB，NB中并没有MDL
+	FwpsAllocateNetBufferAndNetBufferList(nblPoolHandle, 0, 0, NULL, 0, 0, &CopyLayerdata);
+	currentNB = NET_BUFFER_LIST_FIRST_NB(CopyLayerdata);
+	
+	
+
+	UINT TCPIPHeadersLen = IPHeaderLen + TCPHeaderLen;
+	ULONG MSSandTCPIPHeader = randomMSS + TCPIPHeadersLen;
+	ULONG currentMDLSize = MSSandTCPIPHeader;
+
+	if (ipHeaderInfo->Flags == 1 || ipHeaderInfo->FragmentOffset > 0)
+		goto EXIT; 
+
+	UCHAR ipFlags = 0;
+	if (ipHeaderInfo->Flags == 2)
+		ipFlags = 0b01000000;
+
+	// NdisCopyFromNetBufferToNetBuffer函数要用的参数变量
+	ULONG bytestocopy = 0;
+	ULONG bytescopied = 0;
+	ULONG DestionationOffset = 0;
+	ULONG SourceOffset = 0;
+
+	while (remainDataLen) {
+		
+		if (remainDataLen != payloadLen)
+		{	// 此时不是第一次进入循环，先分配新的NB
+			PNET_BUFFER newNB = NdisAllocateNetBuffer(nbPoolHandle_datasize0, NULL, 0, 0);
+			NET_BUFFER_NEXT_NB(currentNB) = newNB;
+			currentNB = newNB;
+		}
+		// 先给当前NB分配一个足够大的MDL
+		currentMDLSize = remainDataLen > randomMSS ? MSSandTCPIPHeader : (remainDataLen + TCPIPHeadersLen);
+
+		if (currentMDLSize > 0) {
+			status = NdisRetreatNetBufferDataStart(currentNB,currentMDLSize,0,NULL);
+			if (!NT_SUCCESS(status))
+				goto EXIT;
+		}
+		// 分配完MDL后 要进行拷贝，第一片的拷贝直接从IP头开始
+		if (remainDataLen == payloadLen)	// remainDataLen还是原值，说明还没开始第一次分配
+		{
+			bytestocopy = MSSandTCPIPHeader;	
+			DestionationOffset = 0;
+			SourceOffset = 0;
+			NdisCopyFromNetBufferToNetBuffer(currentNB, DestionationOffset, bytestocopy, nb, SourceOffset, &bytescopied);
+			if (bytescopied != bytestocopy)
+				goto EXIT;
+
+			currentNB->DataLength = bytestocopy;
+			currentNB->CurrentMdl->ByteCount = currentNB->DataLength;	//currentNB->CurrentMdlOffset; 该值为0
+			remainDataLen -= randomMSS;
+
+			//	updatae ipH
+			PUCHAR ipHeader = NdisGetDataBuffer(currentNB, IPHeaderLen, NULL, 1, 0);
+
+			//updateIPHeader(ipHeader, IPHeaderLen, MSSandTCPIPHeader, ipFlags, 0);
+
+			updateTCPchecksum(ipHeader, ipHeader + IPHeaderLen, ipHeader + TCPIPHeadersLen, TCPHeaderLen, randomMSS);
+
+		}
+		else {	// 如果不是第一片，需要先copy头部，然后从已经拷贝完的payload位置开始继续拷贝
+
+			bytestocopy = TCPIPHeadersLen;
+			DestionationOffset = 0;
+			SourceOffset = 0;
+			NdisCopyFromNetBufferToNetBuffer(currentNB, DestionationOffset, bytestocopy, nb, SourceOffset, &bytescopied);
+			if (bytescopied != bytestocopy)
+				goto EXIT;
+
+			bytestocopy = currentMDLSize - TCPIPHeadersLen;
+			DestionationOffset = TCPIPHeadersLen;							// des：跳过目的NB的TCP和IP首部
+			SourceOffset = TCPIPHeadersLen + payloadLen - remainDataLen;	// src：跳过源NB的首部和已经复制过的payload
+			NdisCopyFromNetBufferToNetBuffer(currentNB, DestionationOffset, bytestocopy, nb, SourceOffset, &bytescopied);
+			if (bytescopied != bytestocopy)			// 判断复制是否成功
+				goto EXIT;
+
+			currentNB->DataLength = currentMDLSize;
+			currentNB->CurrentMdl->ByteCount = currentNB->DataLength;	//currentNB->CurrentMdlOffset; 该值为0
+			
+			PUCHAR ipHeader = NdisGetDataBuffer(currentNB, IPHeaderLen, NULL, 1, 0);
+
+			//updateIPHeader(ipHeader, IPHeaderLen, currentMDLSize, ipFlags, 0);
+
+			updateTCPSeqNumber(ipHeader+IPHeaderLen, payloadLen - remainDataLen);
+
+			updateTCPchecksum(ipHeader, ipHeader + IPHeaderLen, ipHeader + TCPIPHeadersLen, TCPHeaderLen, bytestocopy);
+
+			remainDataLen -= bytescopied;
+		}
+	}
