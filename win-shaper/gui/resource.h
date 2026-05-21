168369243cd1f5fd63419212df93cf2e19c39409
lunan0320 <lunan0320@users.noreply.github.com>
2026-05-21 15:38:05 +0800

    Add GUI resource identifiers

---
+++ win-shaper/gui/resource.h
@@ -0,0 +1,41 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by gui.rc
+//
+#define IDC_DELETE                      4
+#define IDC_EDIT                        5
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_GUI_DIALOG                  102
+#define IDR_MAINFRAME                   128
+#define IDD_CUSTOM_PROFILES             131
+#define IDD_DIALOG1                     133
+#define IDD_CUSTOM_PROFILE              133
+#define RC_SHAPER_32                    300
+#define RC_SHAPER_64                    301
+#define IDC_CONNECTION_PROFILES         1001
+#define IDC_ENABLE                      1002
+#define IDC_SYSLINK1                    1004
+#define IDC_LIST1                       1005
+#define IDC_ADD                         1007
+#define IDC_INBOUND_QUEUE               1008
+#define IDC_OUTBOUND_QUEUE              1009
+#define IDC_NAME                        1009
+#define IDC_IN_BPS                      1010
+#define IDC_OUT_BPS                     1011
+#define IDC_RTT                         1012
+#define IDC_PLR                         1013
+#define IDC_IN_BUFF_LEN                 1014
+#define IDC_OUT_BUFF_LEN                1015
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
