e4a4326370313bf3a0f080b38db7eb355ececd57
lunan0320 <lunan0320@users.noreply.github.com>
2026-05-21 15:20:39 +0800

    Add WFP callout declarations

---
+++ win-shaper/driver/wfp_callout.h
@@ -0,0 +1,33 @@
+#pragma once
+
+// WFP Callout entry points
+void ShaperClassify(
+    _In_ const FWPS_INCOMING_VALUES* inFixedValues,
+    _In_ const FWPS_INCOMING_METADATA_VALUES* inMetaValues,
+    _Inout_opt_ void* layerData,
+    _In_opt_ const void* classifyContext,
+    _In_ const FWPS_FILTER* filter,
+    _In_ UINT64 flowContext,
+    _Inout_ FWPS_CLASSIFY_OUT* classifyOut);
+
+NTSTATUS ShaperNotify(
+    _In_ FWPS_CALLOUT_NOTIFY_TYPE notifyType,
+    _In_ const GUID* filterKey,
+    _Inout_ const FWPS_FILTER* filter);
+
+
+
+void IPClassify(
+    _In_ const FWPS_INCOMING_VALUES* inFixedValues,
+    _In_ const FWPS_INCOMING_METADATA_VALUES* inMetaValues,
+    _Inout_opt_ void* layerData,
+    _In_opt_ const void* classifyContext,
+    _In_ const FWPS_FILTER* filter,
+    _In_ UINT64 flowContext,
+    _Inout_ FWPS_CLASSIFY_OUT* classifyOut);
+
+    
+NTSTATUS IPNotify(
+    _In_ FWPS_CALLOUT_NOTIFY_TYPE notifyType,
+    _In_ const GUID* filterKey,
+    _Inout_ const FWPS_FILTER* filter);
